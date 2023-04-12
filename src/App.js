b434ce76a64473f196e74bfbe9ef1c92d156da6d
Sawan <itissaavn@gmail.com>
2023-04-12 15:37:10 +0530

    deleted accordian

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import Leaderboard from "./components/Leaderboard/Leaderboard";
 import Profile from "./components/Profile/Profile";
 import "./App.css";
 import React from "react";
-import Accordian from "./components/Accordian/Accordian";
 
 function App() {
   return (
     <React.Fragment>
-      {/* <ProjectBlog /> */}
-      {/* <Leaderboard /> */}
-      <Accordian />
-      {/* <Profile /> */}
+      <ProjectBlog />
+      <Leaderboard />
+      <Profile />
     </React.Fragment>
   );
 }
